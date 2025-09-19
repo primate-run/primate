a4f67efcd4fb42f103512e5698400b80695cbf86
jtenner <tenner.joshua@gmail.com>
2025-09-19 14:39:15 -0400

    move tests, fix tsconfig, remove debugger

--- apps/grain/test/body.ts
+++ apps/grain/test/body.ts
@@ -2,6 +2,7 @@ import type Dict from "@rcompat/type/Dict";
 import test from "primate/test";
 
 const host = "http://localhost:10004";
+
 /**
  * Build a Request with correct encoding for the given content type.
  * - text/plain: pass string as-is
@@ -9,7 +10,7 @@ const host = "http://localhost:10004";
  * - application/x-www-form-urlencoded: URLSearchParams encode
  * - multipart/form-data: use FormData; DO NOT set Content-Type
  */
-function request(path: string, body: any, contentType = "text/plain", method = "POST") {
+function request(path, body, contentType = "text/plain", method = "POST") {
   const url = `${host}${path}`;
   const headers = {} as Dict<string>;
   let payload = body;
@@ -50,16 +51,82 @@ function request(path: string, body: any, contentType = "text/plain", method = "
 
   return new Request(url, { body: payload, headers, method });
 }
-// 6 bytes: 0xDE 0xAD 0xBE 0xEF 0x00 0x01
-const bytes = new Uint8Array([0xde, 0xad, 0xbe, 0xef, 0x00, 0x01]);
 
 test.post(
-  request("/body/binary", bytes, "application/octet-stream"),
+  request("/body/text", "Hi!", "text/plain"),
   response => {
-    response.body.equals({
-      head: [222, 173, 190, 239], // first 4 bytes in decimal
-      size: bytes.byteLength,
-      type: "application/octet-stream",
-    });
+    response.body.equals("Hi!");
   },
 );
+
+test.post(
+  request("/body/json", { baz: 1, foo: "bar" }, "application/json"),
+  response => {
+    response.body.equals({ baz: 1, foo: "bar" });
+  },
+);
+
+test.post(
+  request(
+    "/body/form",
+    { baz: "1", foo: "bar" },
+    "application/x-www-form-urlencoded",
+  ),
+  response => {
+    response.body.equals({ baz: "1", foo: "bar" });
+  },
+);
+
+test.post(
+  request(
+    "/body/multipart",
+    { baz: "1", foo: "bar" },
+    "multipart/form-data",
+  ),
+  response => {
+    response.body.equals({ baz: "1", foo: "bar" });
+  },
+);
+
+{
+  const fd = new FormData();
+  fd.append("baz", "1");
+  fd.append("foo", "bar");
+  fd.append(
+    "greeting",
+    new File(["Hello from a file!"], "greeting.txt", { type: "text/plain" }),
+  );
+  test.post(
+    request("/body/multipart-file", fd, "multipart/form-data"),
+    async response => {
+      const form = response.body;
+      // Expected server-side parsing: fields + file info
+      form.equals({
+        baz: 1,
+        foo: "bar",
+        greeting: {
+          content: "Hello from a file!",
+          name: "greeting.txt",
+          size: 18,
+          type: "text/plain",
+        },
+      });
+    },
+  );
+}
+
+{
+  // 6 bytes: 0xDE 0xAD 0xBE 0xEF 0x00 0x01
+  const bytes = new Uint8Array([0xde, 0xad, 0xbe, 0xef, 0x00, 0x01]);
+
+  test.post(
+    request("/body/binary", bytes, "application/octet-stream"),
+    response => {
+      response.body.equals({
+        head: [222, 173, 190, 239], // first 4 bytes in decimal
+        size: bytes.byteLength,
+        type: "application/octet-stream",
+      });
+    },
+  );
+}
