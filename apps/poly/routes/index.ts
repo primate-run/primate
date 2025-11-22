102bb66c3668e4b3921c6fcae0270555f17eeeea
terrablue <102580937+terrablue@users.noreply.github.com>
2025-11-22 18:14:22 +0100

    remove poly

--- apps/poly/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import response from "primate/response";
-import route from "primate/route";
-
-const posts = [{
-  id: 1,
-  title: "First post",
-}];
-
-route.get(() => response.view("Index.poly", { posts }));
--- apps/poly/views/layout.svelte
+++
@@ -1,8 +0,0 @@
-<ul>
-  <li>
-    <a href="/">overview</a>
-  </li>
-</ul>
-<div>
-  <slot></slot>
-</div>
