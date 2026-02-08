f5ba81bb0352d7d6f2fb79312517a33925742f38
terrablue <102580937+terrablue@users.noreply.github.com>
2026-02-08 01:41:45 +0100

    apps: use new store imports / orm keys

--- apps/db/stores/User.ts
+++ apps/db/stores/User.ts
@@ -3,7 +3,7 @@ import key from "primate/orm/key";
 import store from "primate/orm/store";
 
 export default store({
-  id: key.primary(p.string),
+  id: key.primary(p.u32),
   name: p.string,
   age: p.u8.range(0, 120),
   lastname: p.string.optional(),
