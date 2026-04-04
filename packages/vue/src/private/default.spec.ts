bd8a44509c07087204a9c5691c56de65bfaf1f65
terrablue <102580937+terrablue@users.noreply.github.com>
2026-04-04 18:52:34 +0200

    vue: process style tags (#258)

--- packages/vue/src/private/default.spec.ts
+++ packages/vue/src/private/default.spec.ts
@@ -1,3 +1,13 @@
 import test from "@primate/test";
 
 test.frontend(import.meta.dirname);
+
+const browser = test.setup(import.meta.dirname);
+
+test.case("/style", async assert => {
+  await using tab = await browser.open();
+  await tab.goto("/style");
+  const el = tab.select("#hello") as Element;
+  const color = (tab.window as any).getComputedStyle(el).color;
+  assert(color).equals("red");
+});
