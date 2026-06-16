a3e0ca43d3a91d4ad651c8b873d510663c9b485c
terrablue <102580937+terrablue@users.noreply.github.com>
2026-06-16 07:25:19 +0200

    apps/marko: switch to env file for port

--- apps/marko/config/app.ts
+++ apps/marko/config/app.ts
@@ -8,9 +8,6 @@ const ssr = env.try("SSR") !== "0";
 const csr = env.try("CSR") !== "0";
 
 export default config({
-  http: {
-    port: 10675, // 10MRK
-  },
   modules: [
     marko({ ssr, csr }),
   ],
